b41ede70399b9bc39b6b7556746a55bc8a684673
nnnyyy <nnnyyy@nate.com>
2022-03-26 02:02:20 +0900

    면접 사이트 작업 중

---
+++ ivhelper/src/views/RootPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="root">
+      <div class="t1">어떤 면접을 준비 중이신가요?</div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'RootPage'
+});
+</script>
+
+
+<style lang="scss" scoped>
+@import "@/assets/styles/anims/fadein.scss";
+
+.root {    
+    display: flex;
+    flex-direction: column;
+    justify-content: center;   
+    align-items: center;;
+    height: 100%;
+}
+
+div.t1 {
+    @include font_temp(3);
+    @include effect_fadein(0s, 3s);
+}
+</style>
--- ivhelper/vue.config.js
+++ ivhelper/vue.config.js
@@ -1,4 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  css: {
+    loaderOptions: {
+      sass: {
+        additionalData: `
+        @import "@/assets/styles/init.scss";
+        @import "@/assets/styles/_variables.scss";
+        @import "@/assets/styles/_mixin.scss";
+        `
+      }
+    }
+  }
 })
